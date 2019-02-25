a446377fc6a41361821fa1083258fd8b4ece5c81
Hano <hinsul3113@gmail.com>
2019-02-24 20:24:33 -0800

    Scaffolded 5 stream components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+
 
 class App extends React.Component {
   render() {
-    return <div>App</div>
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/show" component={StreamShow} />
+            <Route path="/streams/new" component={StreamCreate} />
+            <Route path="/streams/edit" component={StreamEdit} />
+            <Route path="/streams/delete" component={StreamDelete} />
+          </div>
+        </BrowserRouter>
+      </div>
+    )
   }
 };
 
