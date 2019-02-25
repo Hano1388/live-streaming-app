ad920c56471d41d6b3b0e4e6c8c24103cc4e13f9
Hano <hinsul3113@gmail.com>
2019-02-24 20:58:56 -0800

    created Header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
+import Header from './Header';
 
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="ui container">
         <BrowserRouter>
           <div>
+            <Header />
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/show" component={StreamShow} />
             <Route path="/streams/new" component={StreamCreate} />
