e19e2abc28246921c87074f2e6e71b847f3218c3
Hano <hinsul3113@gmail.com>
2019-02-27 03:45:10 -0800

    wrote createStream action and hooked it up to our SreamCreate component to create a stream

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
 
@@ -33,8 +35,8 @@ class StreamCreate extends React.Component {
     );
   }
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
   }
   render() {
     return (
@@ -60,7 +62,11 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, {
+  createStream
+})(formWrapped);
