df2261067d3bf7a370a1acb2699fb8b8e91a806c
Hano <hinsul3113@gmail.com>
2019-02-27 01:58:50 -0800

    added redux-form and a form to create streams

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -56,7 +56,6 @@ class GoogleAuth extends React.Component {
     }
   }
   render() {
-    console.log(this.props);
     return <div>{this.renderAuthButton()}</div>
   }
 };
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>
+class StreamCreate extends React.Component {
+  renderInput({ input, label }) {
+    // return(
+    //   <input
+    //     onChange={formProps.input.onChange}
+    //     value={formProps.input.value}
+    //   />
+    // );
+
+    // Equivalent to the above return statement
+    // return <input {...formProps.input} />
+    // or we can destructure out input from formProps and just use input
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+  render() {
+    return (
+      <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field name="description" component={this.renderInput} label="Enter Description" />
+        <button className="ui button primary">Create Stream</button>
+      </form>
+    );
+  }
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
