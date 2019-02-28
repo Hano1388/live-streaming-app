bd97fbe959454a4ab45b81ebce46d8b7024681bb
Hano <hinsul3113@gmail.com>
2019-02-27 23:21:23 -0800

    created a modal to handle delete stream

---
+++ client/src/components/Modal.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import history from '../history';
+
+const Modal = props => {
+  return ReactDOM.createPortal(
+    <div className="ui dimmer modals visible active" onClick={() => history.push('/')}>
+      <div className="ui standard modal visible active">
+        <div className="header">Delete Stream</div>
+        <div className="content">Are you sure you want to delete this stream?</div>
+        <div className="actions">
+          <button className="ui negative button">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
+      </div>
+    </div>,
+    document.querySelector('#modal')
+  );
+};
+
+export default Modal;
