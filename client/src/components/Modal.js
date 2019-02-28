5413a1c397ce9c9690c1690c9fd39a9dbfa21c93
Hano <hinsul3113@gmail.com>
2019-02-27 23:22:30 -0800

    fixed event propagation on modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -5,7 +5,7 @@ import history from '../history';
 const Modal = props => {
   return ReactDOM.createPortal(
     <div className="ui dimmer modals visible active" onClick={() => history.push('/')}>
-      <div className="ui standard modal visible active">
+      <div className="ui standard modal visible active" onClick={(e) => e.stopPropagation()}>
         <div className="header">Delete Stream</div>
         <div className="content">Are you sure you want to delete this stream?</div>
         <div className="actions">
